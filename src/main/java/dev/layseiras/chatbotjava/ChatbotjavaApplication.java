0ba025f86b428da2758ef8b004a268ee226d67b8
Layseiras <layseirasdev@outlook.com>
2025-04-29 16:27:51 -0300

    Initial commit

---
+++ src/main/java/dev/layseiras/chatbotjava/ChatbotjavaApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.chatbotjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/chatbotjava/ChatbotjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.chatbotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
