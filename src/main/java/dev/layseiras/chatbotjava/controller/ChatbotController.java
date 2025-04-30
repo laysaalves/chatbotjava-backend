cfa8f7961f4b47b0c9717d5cb4d482b117fd186e
Laysa <laysa.developer@gmail.com>
2025-04-29 21:15:06 -0300

    feat: add final post request for user and chatbot interaction

--- src/main/java/dev/layseiras/chatbotjava/controller/ChatbotController.java
+++ src/main/java/dev/layseiras/chatbotjava/controller/ChatbotController.java
@@ -2,14 +2,14 @@ package dev.layseiras.chatbotjava.controller;
 
 import dev.layseiras.chatbotjava.dtos.*;
 import dev.layseiras.chatbotjava.service.ChatbotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/chat")
+@CrossOrigin(origins = "*")
 public class ChatbotController {
 
     private final ChatbotService service;
@@ -19,12 +19,7 @@ public class ChatbotController {
     }
 
     @PostMapping
-    public ResponseEntity<ChatbotResponse> interact(@RequestBody ChatbotRequest request) {
-        try {
-            ChatbotResponse response = this.service.interactWithChat(request.userInput());
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+    public Mono<String> generateEmail(@RequestBody ChatbotRequest request) {
+        return service.getChatbotOutput(request);
     }
 }
